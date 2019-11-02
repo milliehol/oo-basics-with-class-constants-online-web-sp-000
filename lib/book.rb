c489e7a40560a5e181a2eba7feee042b2ff11383
Carla Holcomb <carla.programmer@gmail.com>
2019-11-02 15:58:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title, :genre
+  attr_reader :title
 
   GENRES = []
 
@@ -8,13 +8,13 @@ class Book
     @title = title
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
-  def genre=(genre)
-   @genre = genre
-   GENRES << genre
- end
 
 end
