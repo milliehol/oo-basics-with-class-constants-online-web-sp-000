621a3ab91e2cf69ddeec7ee967cfdb9cbdb45df4
Carla Holcomb <carla.programmer@gmail.com>
2019-11-02 16:00:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,21 +1,16 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :brand
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   BRANDS = []
-
-  def brand=(brand)
-   @brand = brand
-   BRANDS << brand
- end
 
   def initialize(brand)
     @brand = brand
   end
 
-  def brand
-    self.condition = "clear"
-    self.size = "3"
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
   def cobble
